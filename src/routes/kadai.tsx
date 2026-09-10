aa5634806efe55e8152ff13a07d41779594719e9
mypovbn-pixel <mypov.bn@gmail.com>
2026-09-10 14:30:19 +0800

    feat: add KADAI landing route

---
+++ src/routes/kadai.tsx
@@ -0,0 +1,3 @@
+import { createFileRoute } from "@tanstack/react-router";
+import KadaiLanding from "@/kadai/pages/KadaiLanding";
+export const Route=createFileRoute("/kadai")({component:KadaiLanding});
